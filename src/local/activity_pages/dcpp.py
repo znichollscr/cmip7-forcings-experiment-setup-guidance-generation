6906d665d1f8fbb3a09855697fd46d13f44aa234
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-06-18 14:32:29 +1000

    Prepare for co-chair review

--- src/local/activity_pages/dcpp.py
+++ src/local/activity_pages/dcpp.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from local.guidance import ExperimentPage
-from local.mip_co_chair_review import get_pending_review_aft_experiments
+from local.mip_co_chair_review import get_complete_review_aft_experiments
 from local.rendering import block
 
 DCPP_EXPERIMENT_PAGES: tuple[ExperimentPage, ...] = (
@@ -29,6 +29,6 @@ DCPP_EXPERIMENT_PAGES: tuple[ExperimentPage, ...] = (
         #         for v in HISTORICAL_FORCINGS_SPECIFICATION.specific_forcings
         #     ),
         # ),
-        mip_co_chair_review=get_pending_review_aft_experiments("dcpp"),
+        mip_co_chair_review=get_complete_review_aft_experiments("dcpp"),
     ),
 )
--- src/local/mip_co_chair_review.py
+++ src/local/mip_co_chair_review.py
@@ -79,7 +79,7 @@ def get_complete_review_aft_experiments(activity: str) -> CompleteReview:
         # "cfmip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/185",
         # "cmip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/183",
         # "damip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/188",
-        # "dcpp": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/207",
+        "dcpp": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/207",
         # "geomip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/189",
         # "lmip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/190",
         # "pmip": "https://github.com/WCRP-CMIP/cmip7-guidance/issues/191",
